e8c32d43fe734598c6cda1dba2519910f3548e8d
nguyenha1111 <nguyenthanhha.html@gmail.com>
2023-01-15 14:54:06 +0700

    learn babel-plugin-module-resolver customize-cra

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import myAlert from '~/myAlert'
+
+
+
+function Button() {
+    return(
+        <button onClick={myAlert}>Click me!</button>
+    )
+}
+export default Button
---
+++ src/myAlert.js
@@ -0,0 +1,4 @@
+const myAlert = () => {
+    alert('hello world')
+}
+export default myAlert
